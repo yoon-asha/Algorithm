79db3c12e613a859e5d3895d8325620a9f982d60
yoon-asha <yoon.asha97@gmail.com>
2022-07-28 01:05:54 +0900

    0727 공부

--- inflean_algorithm/section2/5.find_class.js
+++ inflean_algorithm/section2/5.find_class.js
@@ -14,3 +14,21 @@
 ▣ 출력예제 1 
 43215
  */
+const solution = (arr) => {
+  
+}
+
+// 선생님 답
+function solution(arr){  
+  let n=arr.length;
+  let answer=Array.from({length:n}, ()=>1);
+  for(let i=0; i<n; i++){
+      for(let j=0; j<n; j++){
+          if(arr[j]>arr[i]) answer[i]++;
+      }
+  }             
+  return answer;
+}
+
+let arr=[87, 89, 92, 100, 76];
+console.log(solution(arr));
