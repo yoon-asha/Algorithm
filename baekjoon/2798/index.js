cac2829f814067718f81fe386fee5a7f2b21ed6e
yoon-asha <asha_@kakao.com>
2022-09-08 17:50:46 +0900

    0908 알고리즘

---
+++ baekjoon/2798/index.js
@@ -0,0 +1,63 @@
+
+/**
+ * 문제
+카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 카드의 합이 21을 넘지 않는 한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 블랙잭은 카지노마다 다양한 규정이 있다.
+
+한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+
+김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+
+이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+
+N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+
+입력
+첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+
+합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+
+출력
+첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+ * 
+1번 입력 답 21
+5 21
+5 6 7 8 9
+
+2번 입력 답 497
+10 500
+93 181 245 214 315 36 185 138 216 295
+ */
+
+
+
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split("\n");
+input[0] = input[0].split(" ");
+const cards = input[1].split(" ").map((element) => Number(element));
+const N = Number(input[0][0]); // N은 총 카드의 개수
+const M = Number(input[0][1]); // M은 3개의 숫자의 합과 가장 가까운 목표 값
+
+const solution = (n, m, arr) => {
+  console.log(arr)
+  console.log(n)
+  console.log(m)
+  let answer = 0;
+
+  product.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]));
+  for (let i = 0; i < n; i++) {
+    let money = m - (product[i][0] / 2 + product[i][1]);
+    let cnt = 1;
+    for (let j = 0; j < n; j++) {
+      if (j !== i && (product[j][0] + product[j][1]) > money) break;
+      if (j !== i && (product[j][0] + product[j][1]) <= money) {
+        money -= (product[j][0] + product[j][1]);
+        cnt++;
+      }
+    }
+    answer = Math.max(answer, cnt);
+  }
+  return answer;
+}
+
+console.log(solution(N, M, cards))
