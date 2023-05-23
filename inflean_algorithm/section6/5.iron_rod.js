9c23f700690e7f3eee2e2f6fc70b3d675924341f
yoon-asha <yoon.asha97@gmail.com>
2023-05-23 15:27:59 +0900

    230523 study

--- inflean_algorithm/section6/5.iron_rod.js
+++ inflean_algorithm/section6/5.iron_rod.js
@@ -38,3 +38,20 @@
 24
 
  */
+
+function solution(str) {
+  let stack = []
+  let answer = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') stack.push(str[i])
+    else {
+      stack.pop()
+      if (str[i - 1] === '(') answer += stack.length
+      else answer += 1
+    }
+  }
+  return answer
+}
+
+console.log(solution('()(((()())(())()))(())')) // 17
+console.log(solution('(((()(()()))(())()))(()())')) // 24
